eda2df0a71e1d12231705f956749e67e853e9459
Hasmelash95 <hermonasmelash@yahoo.com>
2022-08-13 18:40:50 +0000

    Refactor update worksheet functions

--- run.py
+++ run.py
@@ -52,15 +52,6 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with list data provided
-    """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully!\n")
-
 def calculate_surplus(sales_row):
     """
     Compare sales with stock and calculate surplus
@@ -78,14 +69,12 @@ def calculate_surplus(sales_row):
     
     return surplus_data
 
-def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with calculated data
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Sales worksheet updated successfully!\n")
+def update_worksheet(data, worksheet):
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfuly!\n")
+
 
 def main():
     """
@@ -93,9 +82,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus = calculate_surplus(sales_data)
-    update_surplus_worksheet(new_surplus)
+    update_worksheet(new_surplus, "surplus")
 
 print("Welcome to love sandwiches data automation!")
 main()
